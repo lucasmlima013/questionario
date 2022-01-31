7a7dbee2e4889d50e8e0363ebad13162db3ecf8e
lucasmlima013 <lucasmlima@gmail.com>
2022-01-31 11:24:01 -0300

    primeira entrega

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+   res.render("index");
+});
+
+app.get("/perguntar", (req,res)=>{
+    res.render("perguntar");
+});
+
+app.listen(8080,()=>{console.log("App rodando");});

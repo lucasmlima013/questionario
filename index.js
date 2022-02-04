c54b09b52f6b4785999bac4c0a416342b4624fc5
lucasmlima013 <lucasmlima@gmail.com>
2022-02-04 08:48:50 -0300

    Criando conexão com BD

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+//database
+const conn = require("./database/database");
 
+conn
+    .authenticate()
+    .then(() => {
+        console.log("Conexao realizada com o BD")
+    })
+    .catch((msgErro) =>{
+        console.log(msgErro);
+    });
+
+
+//usando express com EJS
 app.use(express.static(__dirname + '/public'));
-
 app.set('view engine', 'ejs');
+
+//Configuração BodyParser
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", (req, res) => {
    res.render("index");
@@ -11,6 +27,13 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req,res)=>{
     res.render("perguntar");
+});
+
+app.post("/salvarpergunta",(req,res)=>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+    res.send("Formulário Recebido" + titulo +" " + descricao);
 });
 
 app.listen(8080,()=>{console.log("App rodando");});

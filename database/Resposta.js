8acd0317c7a69ef114d94a0a04f7f5813265f700
lucasmlima013 <lucasmlima@gmail.com>
2022-02-05 14:34:59 -0300

    Criando conexão com BD

---
+++ database/Resposta.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+const conn = require("./database")
+
+const Resposta = conn.define('resposta',{
+    titulo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    
+    descricao: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+});
+
+Pergunta.sync({force: false}).then(() => {});
+
+module.exports = Resposta;
